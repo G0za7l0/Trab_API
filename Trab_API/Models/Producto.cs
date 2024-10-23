ba4daf62873803f1bd4fef1e84d7549c971ff341
G0za7l0 <gonzalosebastiam123xd@gmail.com>
2024-10-23 17:29:24 -0600

    first commit

---
+++ Trab_API/Models/Producto.cs
@@ -0,0 +1,13 @@
+﻿namespace Trab_API.Models
+{
+    public class Producto
+    {
+        public int ID_Producto{ get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+        private string Descripcion { get; set; } = null!;
+
+        private decimal Precio { get; set; } 
+    }
+}
